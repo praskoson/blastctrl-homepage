a25658656599b21ac94f182e7d7146d2dd7a301b
ppetar <prasinar@gmail.com>
2022-02-19 14:12:24 +0100

    Removed logging and some comments

--- src/js/captcha.js
+++ src/js/captcha.js
@@ -1,6 +1,5 @@
 const inputFields = document.querySelectorAll('form[name="contact-form"] input, textarea');
 
-console.log(inputFields);
 inputFields.forEach(i => {
   i.addEventListener('focus', (e) => {
     const captcha = document.querySelector('.grecaptcha-badge');
