0a3f00d01320813cedf8771d70fdd461e79e284b
praskoson <prasinar@gmail.com>
2023-08-21 21:09:43 +0200

    animation scrubbing on touchpads

--- src/js/scrolly.js
+++ src/js/scrolly.js
@@ -1,5 +1,19 @@
 import { animate, inView } from "motion";
 
+function throttle(cb, delay = 250) {
+  let shouldWait = false;
+
+  return (...args) => {
+    if (shouldWait) return;
+
+    cb(...args);
+    shouldWait = true;
+    setTimeout(() => {
+      shouldWait = false;
+    }, delay);
+  };
+}
+
 /**
  * @type {import("motion").AnimationControls}
  */
@@ -49,15 +63,44 @@ inView(scrollWrapper, () => {
   };
 });
 
-marquee1.addEventListener("mouseenter", () => {
-  slowStop(control1);
-});
-marquee1.addEventListener("mouseleave", () => {
-  slowStart(control1);
-});
-marquee2.addEventListener("mouseenter", () => {
-  slowStop(control2);
-});
-marquee2.addEventListener("mouseleave", () => {
-  slowStart(control2);
-});
+let isMobile = window.matchMedia("(any-pointer:coarse)").matches;
+
+if (isMobile) {
+  // let start = { x: 0, y: 0 };
+  // function touchStart(event) {
+  //   control2.pause();
+  //   start.x = event.touches[0].pageX;
+  //   start.y = event.touches[0].pageY;
+  // }
+  // function touchMove(event) {
+  //   event.preventDefault();
+  //   let x = start.x - event.touches[0].pageX;
+  //   if (control2.currentTime) control2.currentTime = (control2.currentTime - x / 2000) % duration;
+  // }
+  // marquee2.addEventListener("touchstart", touchStart, false);
+  // marquee2.addEventListener("touchmove", touchMove, false);
+  // marquee2.addEventListener("touchend", () => {
+  //   setTimeout(() => slowStart(control2), 2000);
+  // });
+} else {
+  marquee1.addEventListener("mouseenter", () => {
+    slowStop(control1);
+  });
+  marquee1.addEventListener("mouseleave", () => {
+    slowStart(control1);
+  });
+  marquee2.addEventListener("mouseenter", () => {
+    slowStop(control2);
+  });
+  marquee2.addEventListener("mouseleave", () => {
+    slowStart(control2);
+  });
+
+  marquee1.addEventListener("wheel", (e) => {
+    control1.currentTime += e.deltaX / 100;
+    control1.currentTime = Math.max(control1.currentTime + e.deltaX / 100, duration);
+  });
+  marquee2.addEventListener("wheel", (e) => {
+    control2.currentTime = Math.max(control2.currentTime - e.deltaX / 100, duration);
+  });
+}
